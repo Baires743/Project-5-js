e279d818d9e1a34f5316f7709f0939be3ecf1659
Baires743 <navirbaires@gmail.com>
2023-06-19 10:34:51 +0300

    even & odd numbers

---
+++ index.js
@@ -0,0 +1,11 @@
+let num = prompt ("Por favor ingrese un número:");
+
+while(num > 0 && num !== -0){
+    if(num % 2 == 0){
+        console.log("Es par")
+    } else 
+        console.log("Es impar");
+        break;
+}    
+
+
